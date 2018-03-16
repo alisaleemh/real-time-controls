f6659cfd8e6d2d8461776d16c9261f88ccef2091
Ali Hussain <ali.s1995gmail.com>
2018-03-16 14:36:54 -0400

    lab 5 minor change

---
+++ lab5/ex5_abd.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <mqueue.h>
+#include <errno.h>
+
+struct Msg {char str[20]; unsigned int priority; };
+int errno;
+
+const char *qname = "/myque";
+// Define the name of the MQ, use your
+// last name as part of the name to create
+// a unique name.
+
+sem_t semaphore;
+int SIZE = 10;
+
+
+void *Receiver(void *arg)  // This thread receives msgs from the MQ
+{
+    mqd_t rx;
+    struct mq_attr my_attrs;
+    ssize_t n;
+    int i;
+    int nMsgs;
+    char inBuf[50];
+    unsigned int priority;
+
+    sleep(2);
+
+    rx = mq_open(qname, O_RDONLY);  // Open the MQ for reading only (O_RDONLY)
+
+    if (rx < 0 ) perror("Receiver mq_open:");  // Error checking
+
+    int messageRecieved = 0;
+
+
+
+
+    // Receive all messages in the MQ by calling mq_receive
+
+    while(1) {
+        mq_getattr(rx, &my_attrs);     // Determine no. of msgs currently in the MQ
+        nMsgs = my_attrs.mq_curmsgs;
+        if (nMsgs > 0)
+        {
+            n = mq_receive(rx, inBuf, sizeof(inBuf), &priority);
+            printf("Receiver got <%s>, %d bytes at priority %d\n", inBuf, n, priority);
+            sleep(2);
+            sem_post(&semaphore);
+            inBuf[n] = '\0';
+            if (n < 0) perror("Receiver mq_receive:");  // Error checking
+            messageRecieved++;
+            if (messageRecieved == SIZE) {
+                pthread_exit(NULL);
+            }
+        }
+    }
+
+    mq_close(rx);   // Close the MQ
+    pthread_exit(NULL);
+}
+
+void *Sender(void *arg)  // This thread sends msgs to the MQ
+{
+    int retcode, i;
+    mqd_t tx;
+    struct Msg myMsg;
+    unsigned int msg_priority[10];  // Set up message priorities
+
+
+    for (i=1; i<=SIZE ; i++)
+    {
+        msg_priority[i-1] = i;
+    }
+
+    sleep(2);
+
+    tx = mq_open(qname, O_WRONLY);  // Open the MQ for writing only (O_WRONLY)
+
+    if (tx < 0) perror("Sender mq_open:");  // Error checking
+
+    // Send 5 messages to the MQ by calling mq_send
+
+    for (i = 1; i <= SIZE; i++) {
+        sprintf(myMsg.str, "This is Test msg %d", i);  // Set up message to be sent
+        myMsg.priority = msg_priority[i-1];    // Set up priority of this message
+        retcode = mq_send(tx, myMsg.str, (size_t)strlen(myMsg.str), myMsg.priority);
+        printf("Sender sent %s \n", myMsg.str );
+        sem_wait(&semaphore);
+        sleep(2);
+        if (retcode < 0) perror("Sender mq_send:");  // Error checking
+    }
+
+    mq_close(tx);   // Close the MQ
+
+    pthread_exit(NULL);
+}
+
+int main(void *arg)
+{
+    mqd_t trx;
+    mode_t mode;
+    int oflags;
+    struct mq_attr my_attrs;
+    pthread_t Sender_thread, Receiver_thread;
+
+    sem_init(&semaphore, 0, 1);
+
+
+    my_attrs.mq_maxmsg = 10;      // Set max no of msg in queue to 10 msgs
+    my_attrs.mq_msgsize = 50;     // Set the max msg size to 50 bytes
+
+    oflags = O_CREAT | O_RDWR ;   // Set oflags to create queue (O_CREAT)
+    //  and for read & write (O_RDWR)
+
+    mode = S_IRUSR | S_IWUSR;     // Set mode to allow other threads
+    //  (created by same user) to use the queue
+
+    trx = mq_open(qname, oflags, mode, &my_attrs);  // Create the MQ
+
+    if (trx < 0) perror("Main mq_open:");  // Error checking
+
+
+    printf("Creating Sender thread\n");
+    if (pthread_create(&Sender_thread, NULL, &Sender, NULL) != 0) {
+        printf("Error creating Sender thread.\n");
+        exit(-1);
+    }
+
+
+    printf("Creating Receiver thread\n");
+    if (pthread_create(&Receiver_thread, NULL, &Receiver, NULL) != 0) {
+        printf("Error creating Receiver thread.\n");
+        exit(-1);
+    }
+
+    pthread_join(Sender_thread, NULL);
+    pthread_join(Receiver_thread, NULL);
+
+    mq_unlink(qname);     // Destroy the MQ
+    exit(0);
+}
